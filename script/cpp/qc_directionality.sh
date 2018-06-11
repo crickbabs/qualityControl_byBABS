4e49ea31c8e5b7d8d719672178696fa05718acd2
Nourdine Bah <nourdine.bah@crick.ac.uk>
2018-06-11 15:43:41 +0100

    first commit

--- script/cpp/qc_directionality.sh
+++ script/cpp/qc_directionality.sh
@@ -12,42 +12,14 @@ GLOBIN=$QCTXTDIR/human_globin_ids.txt
 # ----
 
 # ----
-#BED=/camp/stp/babs/working/data/genomes
-#BED=$BED/mus_musculus/ensembl/GRCm38/release-86/gtf
-#BED=$BED/Mus_musculus.GRCm38.86.bed
-# ----
-
-#BAM=/camp/stp/babs/working/bahn/bam/sample01.cutadapt.STAR.genome.sorted.bam
-
-# ----
-#BAM=/camp/stp/babs/working/bahn/projects/hayday/maria.iannitto
-#BAM=$BAM/009_rnaseq_tigit_thymocyte_maturation/picard/Th38POS.dupmarked.bam
-# ----
-
-# ----
 BED=/camp/stp/babs/working/data/genomes
 BED=$BED/homo_sapiens/ensembl/GRCh38/release-86/gtf
 BED=$BED/Homo_sapiens.GRCh38.86.bed
 # ----
 
 # ----
-#BAM=/camp/stp/babs/working/bahn/code/workflow/rnaseq/nflow
-#BAM=$BAM/general/nextflow_single_end/picard/control1.dupmarked.bam
-# ----
-
-# ----
-#BAM=/camp/stp/babs/working/bahn/code/workflow/rnaseq/nflow
-#BAM=$BAM/general/nextflow_paired_end/picard/control1.dupmarked.bam
-# ----
-
-# ----
 BAM=test/multiqc/paired_end/control1.dupmarked.bam
 # ----
-
-# ----
-#BAM=test/multiqc/single_end/control1.dupmarked.bam
-# ----
-
 
 	#-pthread \
 if [[ $1 == "compil" ]]
--- script/cpp/transcript_directionality.sh
+++ script/cpp/transcript_directionality.sh
@@ -4,44 +4,6 @@ module load GCC/6.4.0-2.28
 
 PROG=transcript_directionality
 
-## ---
-#BED=/camp/stp/babs/working/data/genomes
-#BED=/mus_musculus/ensembl/GRCm38/release-86/gtf
-#BED=$BED/Mus_musculus.GRCm38.86.bed
-#BAM=/camp/stp/babs/working/bahn/bam/sample01.cutadapt.STAR.genome.sorted.bam
-## ---
-
-## ---
-#BED=/camp/stp/babs/working/data/genomes
-#BED=$BED/homo_sapiens/ensembl/GRCh38/release-86/gtf
-#BED=$BED/Homo_sapiens.GRCh38.86.bed
-#BAM=/camp/stp/babs/working/bahn/projects/hayday/maria.iannitto
-#BAM=$BAM/009_rnaseq_tigit_thymocyte_maturation/picard/Th38POS.dupmarked.bam
-## ---
-
-## ---
-#BED=/camp/stp/babs/working/data/genomes
-#BED=$BED/homo_sapiens/ensembl/GRCh38/release-86/gtf
-#BED=$BED/Homo_sapiens.GRCh38.86.bed
-#BAM=/camp/stp/babs/working/bahn/code/workflow/rnaseq/nflow/general
-#BAM=$BAM/nextflow_single_end/picard/control1.dupmarked.bam
-## ---
-
-## ---
-#BED=/camp/stp/babs/working/data/genomes
-#BED=$BED/homo_sapiens/ensembl/GRCh38/release-86/gtf
-#BED=$BED/Homo_sapiens.GRCh38.86.bed
-#BAM=/camp/stp/babs/working/bahn/code/workflow/rnaseq/nflow/general
-#BAM=$BAM/nextflow_paired_end/picard/control1.dupmarked.bam
-## ---
-
-## ---
-#BED=/camp/stp/babs/working/data/genomes
-#BED=$BED/homo_sapiens/ensembl/GRCh38/release-86/gtf
-#BED=$BED/Homo_sapiens.GRCh38.86.bed
-#BAM=test/multiqc/single_end/control1.dupmarked.bam
-## ---
-
 # ---
 BED=/camp/stp/babs/working/data/genomes
 BED=$BED/homo_sapiens/ensembl/GRCh38/release-86/gtf
