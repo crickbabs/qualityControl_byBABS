6ef4dea85975776795b6d8c6775134f7c63465c9
Nourdine Bah <nourdine.bah@crick.ac.uk>
2018-07-28 15:48:39 +0100

    first commit

--- script/multiqc/qc-plugin/qc_plugin/templates/__init__.py
+++
@@ -1,4 +0,0 @@
-import os
-
-template_dir = os.path.dirname(__file__)
-template_fn = 'multiqc_report.html'
--- script/multiqc/qc-plugin/qc_plugin/templates/custom/__init__.py
+++
@@ -1,9 +0,0 @@
-import os
-
-template_parent = "default"
-
-template_dir = os.path.dirname(__file__)
-base_fn = "base.html"
-
-copy_files = ["assets"]
-
